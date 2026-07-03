1bf7ccb325d8e0ebb7ee7f745945f1d55c46040a
Dennis Pham <dennis@dennispham.me>
2026-07-03 18:41:48 -0400

    feat(pong): enable nvidia optimus prime

--- modules/hosts/pong.nix
+++ modules/hosts/pong.nix
@@ -74,9 +74,25 @@
               enable = false;
               finegrained = false;
             };
+            prime = {
+              sync.enable = true;
+              intelBusId = "PCI:0:2:0";
+              nvidiaBusId = "PCI:1:0:0";
+            };
             open = true;
             nvidiaSettings = true;
             package = config.boot.kernelPackages.nvidiaPackages.stable;
+          };
+        };
+
+        specialisation = {
+          on-the-go.configuration = {
+            system.nixos.tags = [ "on-the-go" ];
+            hardware.nvidia = {
+              prime.offload.enable = lib.mkForce true;
+              prime.offload.enableOffloadCmd = lib.mkForce true;
+              prime.sync.enable = lib.mkForce false;
+            };
           };
         };
 
